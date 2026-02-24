50d4b9883abb09de7a2ecd3e6ea9b72c43f8a32b
Fynn Kleine <code@onlyfynns.de>
2026-02-24 13:05:42 +0100

    feat: JSON / XML wrappers structure

--- aas-json/src/lib.rs
+++ aas-json/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::{Deserialize, Deserializer, Serialize};
+
+/// Wrapper Struct to serialize/deserialize given struct to/from JSON
+pub struct JSON<T: Json>(T);
+
+impl<'de, T: Json> Deserialize<'de> for JSON<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let str = <&str>::deserialize(deserializer)?;
+        let t = T::from_json(str).map_err(serde::de::Error::custom)?;
+        Ok(Self(t))
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T: Json> Serialize for JSON<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0
+            .to_json()
+            .map(|a| a.serialize(serializer))
+            .map_err(serde::ser::Error::custom)?
     }
+}
+
+pub trait Json: Sized {
+    type Error: std::fmt::Display;
+
+    fn to_json(&self) -> Result<String, Self::Error>;
+
+    fn from_json(json: &str) -> Result<Self, Self::Error>;
 }
--- aas-xml/src/lib.rs
+++ aas-xml/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::{Deserialize, Deserializer, Serialize};
+
+/// Wrapper Struct to serialize/deserialize given struct to/from XML
+pub struct XML<T: Xml>(T);
+
+impl<'de, T: Xml> Deserialize<'de> for XML<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let str = <&str>::deserialize(deserializer)?;
+        let t = T::from_xml(str).map_err(serde::de::Error::custom)?;
+        Ok(Self(t))
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T: Xml> Serialize for XML<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0
+            .to_xml()
+            .map(|a| a.serialize(serializer))
+            .map_err(serde::ser::Error::custom)?
     }
+}
+
+pub trait Xml: Sized {
+    type Error: std::fmt::Display;
+
+    fn to_xml(&self) -> Result<String, Self::Error>;
+
+    fn from_xml(json: &str) -> Result<Self, Self::Error>;
 }
